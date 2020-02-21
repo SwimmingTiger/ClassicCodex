c48be326e197698fe9df130e8e567cacd5ae5944
SwimmingTiger <hu60.cn@gmail.com>
2020-02-22 04:21:34 +0800

    Sycn database with Questie 5.7.1; Add coords for Magrami Spectre

--- db-patches/manual-patch.lua
+++ db-patches/manual-patch.lua
@@ -46,7 +46,36 @@ D[7670]={
 
 if select(4, GetAddOnInfo('MergeQuestieToCodexDB')) then return end
 local D = CodexDB.units.data
+
+if UnitFactionGroup('player') == 'Alliance' then
+  D[13778].coords={
+    {48.5,58.3,2597,0}, --add
+    {50.2,65.3,2597,0}, --add
+    {49.3,84.4,2597,0}, --add
+    {48.3,84.3,2597,0}, --add
+  }
+end
+
 -- Chief Murgut <https://classic.wowhead.com/npc=12918/chief-murgut>
 D[12918].coords={
   {56.4,63.6,331,0},
+}
+
+-- Magrami Spectre <https://classic.wowhead.com/npc=11560/magrami-spectre>
+D[11560].coords={
+  {62.4,91.4,405,300},
+  {63.2,92.2,405,300},
+  {63.4,91.4,405,300},
+  {63.4,92.6,405,300},
+  {63.6,92.4,405,300},
+  {63.8,92.6,405,300},
+  {64.2,91.4,405,300},
+  {64.4,90.4,405,300},
+  {64.6,91.6,405,300},
+  {64.6,92.6,405,300},
+  {64.8,90.4,405,300},
+  {64.8,91.0,405,300},
+  {65.0,89.4,405,300},
+  {65.6,90.2,405,300},
+  {65.6,90.6,405,300},
 }
--- tools/update-db-patches.sh
+++ tools/update-db-patches.sh
@@ -11,15 +11,20 @@ mkdir -p ../db-patches/tmp
 
 {
     cat "$1"
-    echo 'print(CodexDatabasePatch.quest)'
+    echo 'print(table.concat(CodexDatabasePatch.quest, "\n"))'
 } | lua > ../db-patches/quests-patch.lua
 
 {
     cat "$1"
-    echo 'print(CodexDatabasePatch.unitHorde)'
+    echo 'print(table.concat(CodexDatabasePatch.unitHorde, "\n"))'
 } | lua > ../db-patches/tmp/units-horde.lua
 
 {
     cat "$1"
-    echo 'print(CodexDatabasePatch.unitAlliance)'
+    echo 'print(table.concat(CodexDatabasePatch.unitAlliance, "\n"))'
 } | lua > ../db-patches/tmp/units-alliance.lua
+
+cp ../db-patches/tmp/units-horde.lua ../db-patches/units-patch.lua
+
+echo diff db-patches/tmp/units-horde.lua db-patches/tmp/units-alliance.lua
+diff ../db-patches/tmp/units-horde.lua ../db-patches/tmp/units-alliance.lua
