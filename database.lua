ffa79cdccff9ebd4f6230351309a5963ff5fe762
SwimmingTiger <hu60.cn@gmail.com>
2021-05-22 16:37:33 +0800

    Fix an error encountered by draenei and blood elves players

--- database.lua
+++ database.lua
@@ -25,9 +25,11 @@ local bitraces = {
     [16] = "Scourge",
     [32] = "Tauren",
     [64] = "Gnome",
-    [128] = "Troll"
+    [128] = "Troll",
+    [512] = "BloodElf",
+    [1024] = "Draenei"
 }
-  
+
 local bitclasses = {
     [1] = "WARRIOR",
     [2] = "PALADIN",
