17fa7d6aa804316058091f8f5aa1c097e49024d8
SwimmingTiger <hu60.cn@gmail.com>
2022-09-10 06:26:11 +0800

    Questie patch: update to 7.1.0 (WotLK only)

--- tools/update-db-patches-classic.sh
+++ tools/update-db-patches-classic.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 if [ "x$1" = "x" ]; then
     echo "Usage:"
-    echo "    $0 'xxxxxx/World of Warcraft/_classic_/WTF/Account/xxxxxxxxxxxxx#xxx\SavedVariables/MergeQuestieToCodexDB.lua'"
+    echo "    $0 'xxxxxx/World of Warcraft/_classic_era_/WTF/Account/xxxxxxxxxxxxx#xxx\SavedVariables/MergeQuestieToCodexDB.lua'"
     exit 1
 fi
 
@@ -24,7 +24,27 @@ mkdir -p ../ClassicCodex-patch/tmp
     echo 'print(table.concat(CodexDatabasePatch.unitAlliance, "\n"))'
 } | lua > ../ClassicCodex-patch/tmp/units-alliance.lua
 
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.objectHorde, "\n"))'
+} | lua > ../ClassicCodex-patch/tmp/objects-horde.lua
+
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.objectAlliance, "\n"))'
+} | lua > ../ClassicCodex-patch/tmp/objects-alliance.lua
+
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.objectLoc, "\n"))'
+} | lua > ../ClassicCodex-patch/objects-loc.lua
+
 cp ../ClassicCodex-patch/tmp/units-horde.lua ../ClassicCodex-patch/units-questie.lua
 
 echo diff ClassicCodex-patch/tmp/units-horde.lua ClassicCodex-patch/tmp/units-alliance.lua
 diff ../ClassicCodex-patch/tmp/units-horde.lua ../ClassicCodex-patch/tmp/units-alliance.lua
+
+cp ../ClassicCodex-patch/tmp/objects-horde.lua ../ClassicCodex-patch/objects-questie.lua
+
+echo diff ClassicCodex-patch/tmp/objects-horde.lua ClassicCodex-patch/tmp/objects-alliance.lua
+diff ../ClassicCodex-patch/tmp/objects-horde.lua ../ClassicCodex-patch/tmp/objects-alliance.lua
---
+++ tools/update-db-patches-wotlk.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+if [ "x$1" = "x" ]; then
+    echo "Usage:"
+    echo "    $0 'xxxxxx/World of Warcraft/_classic_/WTF/Account/xxxxxxxxxxxxx#xxx\SavedVariables/MergeQuestieToCodexDB.lua'"
+    exit 1
+fi
+
+mkdir -p ../ClassicCodex-patch/tmp
+
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.quest, "\n"))'
+} | lua > ../ClassicCodex-patch/quests-questie-wotlk.lua
+
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.unitHorde, "\n"))'
+} | lua > ../ClassicCodex-patch/tmp/units-horde-wotlk.lua
+
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.unitAlliance, "\n"))'
+} | lua > ../ClassicCodex-patch/tmp/units-alliance-wotlk.lua
+
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.objectHorde, "\n"))'
+} | lua > ../ClassicCodex-patch/tmp/objects-horde-wotlk.lua
+
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.objectAlliance, "\n"))'
+} | lua > ../ClassicCodex-patch/tmp/objects-alliance-wotlk.lua
+
+{
+    cat "$1"
+    echo 'print(table.concat(CodexDatabasePatch.objectLoc, "\n"))'
+} | lua > ../ClassicCodex-patch/objects-loc-wotlk.lua
+
+cp ../ClassicCodex-patch/tmp/units-horde-wotlk.lua ../ClassicCodex-patch/units-questie-wotlk.lua
+
+echo diff ClassicCodex-patch/tmp/units-horde-wotlk.lua ClassicCodex-patch/tmp/units-alliance-wotlk.lua
+diff ../ClassicCodex-patch/tmp/units-horde-wotlk.lua ../ClassicCodex-patch/tmp/units-alliance-wotlk.lua
+
+cp ../ClassicCodex-patch/tmp/objects-horde-wotlk.lua ../ClassicCodex-patch/objects-questie-wotlk.lua
+
+echo diff ClassicCodex-patch/tmp/objects-horde-wotlk.lua ClassicCodex-patch/tmp/objects-alliance-wotlk.lua
+diff ../ClassicCodex-patch/tmp/objects-horde-wotlk.lua ../ClassicCodex-patch/tmp/objects-alliance-wotlk.lua
